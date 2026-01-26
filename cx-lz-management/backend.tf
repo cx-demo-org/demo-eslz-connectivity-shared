ab2498bcff10a499e2677f02b02c0f7d2dee7d88
Puneet V Devadiga <puneetvasu25@gmail.com>
2026-01-26 13:02:09 +0800

    Repo split: remove cx-lz-management (moved to msft-eslz-management)

--- cx-lz-management/backend.tf
+++
@@ -1,5 +0,0 @@
-terraform {
-  backend "local" {
-    path = "C:/LocalApps/GithubWorkspaces/cx-statestore/cx-lz-management/terraform.tfstate"
-  }
-}
--- cx-lz-management/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "management_group_resource_ids" {
-  description = "Management group resource IDs created by the ALZ module."
-  value       = try(module.alz.management_group_resource_ids, null)
-}
