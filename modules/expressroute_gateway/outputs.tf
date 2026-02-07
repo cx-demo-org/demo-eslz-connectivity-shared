24c2c45ca667ad0d7f875f43a8418a32ec00f2ec
Puneet V Devadiga <puneetvasu25@gmail.com>
2026-02-07 13:12:35 +0800

    Make ER gateway outputs shape-safe

--- modules/expressroute_gateway/outputs.tf
+++ modules/expressroute_gateway/outputs.tf
@@ -1,14 +1,31 @@
+locals {
+  gateway_id = try(
+    module.this.resource_id["gateway"],
+    module.this.resource_id[0]
+  )
+
+  gateway_name = try(
+    module.this.resource["gateway"],
+    module.this.resource[0]
+  )
+
+  gateway_object = try(
+    module.this.resource_object["gateway"],
+    module.this.resource_object[0]
+  )
+}
+
 output "id" {
   description = "ExpressRoute Gateway resource ID."
-  value       = module.this.resource_id[0]
+  value       = local.gateway_id
 }
 
 output "name" {
   description = "ExpressRoute Gateway resource name."
-  value       = module.this.resource[0]
+  value       = local.gateway_name
 }
 
 output "resource_object" {
   description = "ExpressRoute Gateway resource object."
-  value       = module.this.resource_object[0]
+  value       = local.gateway_object
 }
