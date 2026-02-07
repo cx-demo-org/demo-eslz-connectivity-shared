b514d6a64b86ab80e716c602c6070fc0b352c718
Puneet V Devadiga <puneetvasu25@gmail.com>
2026-02-07 13:10:18 +0800

    Fix ER gateway outputs indexing

--- modules/expressroute_gateway/outputs.tf
+++ modules/expressroute_gateway/outputs.tf
@@ -1,14 +1,14 @@
 output "id" {
   description = "ExpressRoute Gateway resource ID."
-  value       = module.this.resource_id["gateway"]
+  value       = module.this.resource_id[0]
 }
 
 output "name" {
   description = "ExpressRoute Gateway resource name."
-  value       = module.this.resource["gateway"]
+  value       = module.this.resource[0]
 }
 
 output "resource_object" {
   description = "ExpressRoute Gateway resource object."
-  value       = module.this.resource_object["gateway"]
+  value       = module.this.resource_object[0]
 }
